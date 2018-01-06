6e5d0f40ea61fc59dd6e1efe9342c81dc17d8f65
ajaydarez <ajaydarez@gmail.com>
2018-01-05 23:35:48 -0800

    this is another deployment by ajay!

--- resources/views/showMenTshirt.blade.php
+++ resources/views/showMenTshirt.blade.php
@@ -11,7 +11,7 @@
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link href="./bootstrap-imageupload/dist/css/bootstrap-imageupload.css" rel="stylesheet">
-        <link href="./productListGrid.css/productListGrid.css" rel="stylesheet">
+        <link href="./productListGrid/productListGrid.css" rel="stylesheet">
         <!-- Styles -->
  
     </head>
