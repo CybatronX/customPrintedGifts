8ff9a5b00e8d1e4fd072364d339031af849200b7
ajaydarez <ajaydarez@gmail.com>
2018-01-05 23:34:29 -0800

    this is another deployment by ajay!

--- resources/views/showMenTshirt.blade.php
+++ resources/views/showMenTshirt.blade.php
@@ -11,69 +11,9 @@
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link href="./bootstrap-imageupload/dist/css/bootstrap-imageupload.css" rel="stylesheet">
-        
+        <link href="./productListGrid.css/productListGrid.css" rel="stylesheet">
         <!-- Styles -->
-        <style>
-            /*html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }*/
-
-            body {
-                padding-top: 70px;
-            }
-
-            .imageupload {
-                margin: 20px 0;
-            }
-
-        </style>
+ 
     </head>
     <body>
         <nav class="navbar navbar-inverse navbar-fixed-top">
@@ -99,17 +39,180 @@
             </div>
         </nav>
 
+        <div class="wrapper">
+            <div class="content">
+                <!-- content here -->
+                <div class="product-grid product-grid--flexbox">
+                    <div class="product-grid__wrapper">
+                        <!-- Product list start here -->
+
+                        <!-- Single product -->
+                        <div class="product-grid__product-wrapper">
+                            <div class="product-grid__product">
+                                <div class="product-grid__img-wrapper">         
+                                    <img src="http://i.imgur.com/y83H9n1.jpg" alt="Img" class="product-grid__img" />
+                                </div>
+                                <span class="product-grid__title">Product title</span>
+                                <span class="product-grid__price">1.399€</span>
+                                <div class="product-grid__extend-wrapper">
+                                    <div class="product-grid__extend">
+                                        <p class="product-grid__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis velit itaque odit.</p>
+                                        <span class="product-grid__btn product-grid__add-to-cart"><i class="fa fa-cart-arrow-down"></i> Add to cart</span>              
+                                        <span class="product-grid__btn product-grid__view"><i class="fa fa-eye"></i> View more</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- end Single product -->
+
+                        <div class="product-grid__product-wrapper">
+                            <div class="product-grid__product">
+                                <div class="product-grid__img-wrapper">         
+                                    <img src="https://images.apple.com/euro/macbook/a/screens/specs/images/finish_silver_large.jpg" alt="Img" class="product-grid__img" />
+                                </div>
+                                <span class="product-grid__title">Product title</span>
+                                <span class="product-grid__price">1.399€</span>
+                                <div class="product-grid__extend-wrapper">
+                                    <div class="product-grid__extend">
+                                        <p class="product-grid__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis velit itaque odit.</p>
+                                        <span class="product-grid__btn product-grid__add-to-cart"><i class="fa fa-cart-arrow-down"></i> Add to cart</span>              
+                                        <span class="product-grid__btn product-grid__view"><i class="fa fa-eye"></i> View more</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="product-grid__product-wrapper">
+                            <div class="product-grid__product">
+                                <div class="product-grid__img-wrapper">         
+                                    <img src="https://images.apple.com/euro/macbook/a/screens/specs/images/finish_silver_large.jpg" alt="Img" class="product-grid__img" />
+                                </div>
+                                <span class="product-grid__title">Product title</span>
+                                <span class="product-grid__price">1.399€</span>
+                                <div class="product-grid__extend-wrapper">
+                                    <div class="product-grid__extend">
+                                        <p class="product-grid__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis velit itaque odit.</p>
+                                        <span class="product-grid__btn product-grid__add-to-cart"><i class="fa fa-cart-arrow-down"></i> Add to cart</span>              
+                                        <span class="product-grid__btn product-grid__view"><i class="fa fa-eye"></i> View more</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="product-grid__product-wrapper">
+                            <div class="product-grid__product">
+                                <div class="product-grid__img-wrapper">         
+                                    <img src="https://images.apple.com/euro/macbook/a/screens/specs/images/finish_silver_large.jpg" alt="Img" class="product-grid__img" />
+                                </div>
+                                <span class="product-grid__title">Product title</span>
+                                <span class="product-grid__price">1.399€</span>
+                                <div class="product-grid__extend-wrapper">
+                                    <div class="product-grid__extend">
+                                        <p class="product-grid__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis velit itaque odit.</p>
+                                        <span class="product-grid__btn product-grid__add-to-cart"><i class="fa fa-cart-arrow-down"></i> Add to cart</span>              
+                                        <span class="product-grid__btn product-grid__view"><i class="fa fa-eye"></i> View more</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="product-grid__product-wrapper">
+                            <div class="product-grid__product">
+                                <div class="product-grid__img-wrapper">         
+                                    <img src="https://images.apple.com/euro/macbook/a/screens/specs/images/finish_silver_large.jpg" alt="Img" class="product-grid__img" />
+                                </div>
+                                <span class="product-grid__title">Product title</span>
+                                <span class="product-grid__price">1.399€</span>
+                                <div class="product-grid__extend-wrapper">
+                                    <div class="product-grid__extend">
+                                        <p class="product-grid__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis velit itaque odit.</p>
+                                        <span class="product-grid__btn product-grid__add-to-cart"><i class="fa fa-cart-arrow-down"></i> Add to cart</span>              
+                                        <span class="product-grid__btn product-grid__view"><i class="fa fa-eye"></i> View more</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="product-grid__product-wrapper">
+                            <div class="product-grid__product">
+                                <div class="product-grid__img-wrapper">         
+                                    <img src="https://images.apple.com/euro/macbook/a/screens/specs/images/finish_silver_large.jpg" alt="Img" class="product-grid__img" />
+                                </div>
+                                <span class="product-grid__title">Product title</span>
+                                <span class="product-grid__price">1.399€</span>
+                                <div class="product-grid__extend-wrapper">
+                                    <div class="product-grid__extend">
+                                        <p class="product-grid__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis velit itaque odit.</p>
+                                        <span class="product-grid__btn product-grid__add-to-cart"><i class="fa fa-cart-arrow-down"></i> Add to cart</span>              
+                                        <span class="product-grid__btn product-grid__view"><i class="fa fa-eye"></i> View more</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="product-grid__product-wrapper">
+                            <div class="product-grid__product">
+                                <div class="product-grid__img-wrapper">         
+                                    <img src="https://images.apple.com/euro/macbook/a/screens/specs/images/finish_silver_large.jpg" alt="Img" class="product-grid__img" />
+                                </div>
+                                <span class="product-grid__title">Product title</span>
+                                <span class="product-grid__price">1.399€</span>
+                                <div class="product-grid__extend-wrapper">
+                                    <div class="product-grid__extend">
+                                        <p class="product-grid__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis velit itaque odit.</p>
+                                        <span class="product-grid__btn product-grid__add-to-cart"><i class="fa fa-cart-arrow-down"></i> Add to cart</span>              
+                                        <span class="product-grid__btn product-grid__view"><i class="fa fa-eye"></i> View more</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="product-grid__product-wrapper">
+                            <div class="product-grid__product">
+                                <div class="product-grid__img-wrapper">         
+                                    <img src="https://images.apple.com/euro/macbook/a/screens/specs/images/finish_silver_large.jpg" alt="Img" class="product-grid__img" />
+                                </div>
+                                <span class="product-grid__title">Product title</span>
+                                <span class="product-grid__price">1.399€</span>
+                                <div class="product-grid__extend-wrapper">
+                                    <div class="product-grid__extend">
+                                        <p class="product-grid__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis velit itaque odit.</p>
+                                        <span class="product-grid__btn product-grid__add-to-cart"><i class="fa fa-cart-arrow-down"></i> Add to cart</span>              
+                                        <span class="product-grid__btn product-grid__view"><i class="fa fa-eye"></i> View more</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="product-grid__product-wrapper">
+                            <div class="product-grid__product">
+                                <div class="product-grid__img-wrapper">         
+                                    <img src="https://images.apple.com/euro/macbook/a/screens/specs/images/finish_silver_large.jpg" alt="Img" class="product-grid__img" />
+                                </div>
+                                <span class="product-grid__title">Product title</span>
+                                <span class="product-grid__price">1.399€</span>
+                                <div class="product-grid__extend-wrapper">
+                                    <div class="product-grid__extend">
+                                        <p class="product-grid__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis velit itaque odit.</p>
+                                        <span class="product-grid__btn product-grid__add-to-cart"><i class="fa fa-cart-arrow-down"></i> Add to cart</span>              
+                                        <span class="product-grid__btn product-grid__view"><i class="fa fa-eye"></i> View more</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>      
+                </div>
+            </div>
+
+        </div>
+
         <div class="container">
 
-            <!-- bootstrap-imageupload. -->
-
-            <img src={{$sketchURL}} class = "img-responsive"/>
-
-            <!-- bootstrap-imageupload method buttons. -->
-<!--             <button type="button" id="imageupload-disable" class="btn btn-danger">Disable</button>
-            <button type="button" id="imageupload-enable" class="btn btn-success">Enable</button>
-            <button type="button" id="imageupload-reset" class="btn btn-primary">Reset</button>
- -->
+            <div class="span4">
+                <img src="http://i.imgur.com/y83H9n1.jpg" class = "center-block"/>
+                <!-- <img src={{$sketchURL}} class = "center-block"/> -->
+            </div>
+ 
         </div>
 
         <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="crossorigin="anonymous"></script>
@@ -117,23 +220,6 @@
         <script src="./bootstrap-imageupload/dist/js/bootstrap-imageupload.js"></script>
 
         <script>
-            var $imageupload = $('.imageupload');
-            $imageupload.imageupload();
-
-            // $('#imageupload-disable').on('click', function() {
-            //     $imageupload.imageupload('disable');
-            //     $(this).blur();
-            // })
-
-            // $('#imageupload-enable').on('click', function() {
-            //     $imageupload.imageupload('enable');
-            //     $(this).blur();
-            // })
-
-            // $('#imageupload-reset').on('click', function() {
-            //     $imageupload.imageupload('reset');
-            //     $(this).blur();
-            // });
         </script>
     </body>
 </html>
