2217bdfe80c48645693811547d9a59be8be753cf
ajaydarez <ajaydarez@gmail.com>
2018-01-07 20:59:59 -0800

    this is another deployment by ajay!

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -97,7 +97,7 @@ class UploadController extends Controller {
 			$commandOutput = shell_exec($removeBackgroundCommand);
 			$output .= htmlentities(trim($commandOutput)) . "\n";
 
-			$queryString 	= array('sketchURL' => $this->app['url']->to('/').'/uploads/sketchedFilesBackgroundRemoved/'.$sketchFileName);
+			$queryString 	= array('sketchURL' => url('/').'/uploads/sketchedFilesBackgroundRemoved/'.$sketchFileName);
 
 			return redirect()->route('showMenTshirt', $queryString);
 			// return redirect()->route('showMenTshirt');
