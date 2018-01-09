f2c1c17f5ded5c29048a0b3055ca362f0b3685fd
ajaydarez <ajaydarez@gmail.com>
2018-01-08 16:33:20 -0800

    this is another deployment by ajay!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -100,7 +100,7 @@
             </form>
         </div>
 
-        <a href="#myModal" role="button" class="modalButton btn-large btn-primary" data-toggle="modal">Launch Demo Modal</a>
+        <a href="#myModal" role="button" class="btn modalButton btn-large btn-primary" data-toggle="modal">Launch Demo Modal</a>
 
         <div id="myModal" class="modal fade">
             <div class="modal-dialog">
@@ -134,7 +134,7 @@
     </div>
 
         <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="crossorigin="anonymous"></script>
-        <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script> -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
         <script src="./bootstrap-imageupload/dist/js/bootstrap-imageupload.js"></script>
 
         <script>
