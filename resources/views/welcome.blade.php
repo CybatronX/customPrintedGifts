362944ce0b861283f76d8537f14847148c39804f
ajaydarez <ajaydarez@gmail.com>
2018-01-14 12:59:16 -0800

    this is another deployment by ajay!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -122,12 +122,11 @@
                     <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                       <div class="modal-dialog" role="document">
                         <div class="modal-content">
-                          <div class="modal-body">
-                            <div id="progressBar" sdata-preset="fan" class="ldBar" data-value="0"></div>
-                          </div>
                         </div>
                       </div>
                     </div>
+
+                    <div id="progressBar" sdata-preset="fan" class="ldBar" data-value="0"></div>
     </div>
     </div>
 
