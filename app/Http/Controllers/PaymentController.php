70244611ceb6776ce2200d37e4e1937b5e7add32
Ajay Anandan <ajay@ajay-hv2t.local>
2018-01-29 20:08:55 -0800

    simple echo route

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -6,5 +6,22 @@ use Illuminate\Http\Request;
 
 class PaymentController extends Controller
 {
-    //
+    public function processPayment(){
+      // $token  = $_POST['stripeToken'];
+      // $email  = $_POST['stripeEmail'];
+
+      // $customer = \Stripe\Customer::create(array(
+      //     'email' => $email,
+      //     'source'  => $token
+      // ));
+
+      // $charge = \Stripe\Charge::create(array(
+      //     'customer' => $customer->id,
+      //     'amount'   => 0.99,
+      //     'currency' => 'usd'
+      // ));
+
+      // $stripePublicKey = config('stripeTest.key');
+      echo "Hello";
+    }
 }
